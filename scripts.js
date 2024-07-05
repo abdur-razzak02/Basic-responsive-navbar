3b334cd2d6d67d343d0112bdcf38f32911b17063
Abdur Razzak <abdurrazzakyeamin@gmail.com>
2024-07-05 21:55:05 +0600

    firts commit

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// document.querySelector('form').addEventListener('submit', function(event) {
+//     const name = document.querySelector('#name').value;
+//     const email = document.querySelector('#email').value;
+//     const message = document.querySelector('#message').value;
+
+//     if (!name || !email || !message) {
+//         alert('Please fill in all fields');
+//         event.preventDefault();
+//     }
+// });
